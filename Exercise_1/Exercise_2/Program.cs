76a01db402e24c547ea7e57e19c13d1de2cc9285
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-04-21 13:10:14 +0400

    convert to binary

---
+++ Exercise_1/Exercise_2/Program.cs
@@ -0,0 +1,27 @@
+﻿public class ConvertToBinaryDemo
+{
+    static void Main()
+    {
+        ConvertToBinary();
+    }
+
+    static void ConvertToBinary()
+    {
+        Console.WriteLine("Enter the number in decimal: ");
+        int decimalInput = Convert.ToInt32(Console.ReadLine());
+
+        List<int> remainders = new List<int>();
+
+        while (decimalInput != 0)
+        {
+            remainders.Add(decimalInput % 2);
+            decimalInput /= 2;
+        }
+
+        Console.WriteLine("Your decimal number in binary is: ");
+        for (int i = remainders.Count - 1; i >= 0; i--)
+        {
+            Console.Write(remainders[i]);
+        }
+    }
+}
