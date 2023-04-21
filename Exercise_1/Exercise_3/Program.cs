f19d650f73cf6ba894376c92ab045c410b8249fd
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-04-21 13:30:25 +0400

    method overloading

---
+++ Exercise_1/Exercise_3/Program.cs
@@ -0,0 +1,18 @@
+﻿class Overloading
+{
+    static void Main()
+    {
+        Calculate(2, 2);
+        Calculate(3, 3, 2);
+    }
+
+    static void Calculate(double length, double width)
+    {
+        Console.WriteLine("Area is: " + length * width);
+    }
+
+    static void Calculate(double length, double width, double height)
+    {
+        Console.WriteLine("Capacity is: " + length * width * height);
+    }
+}
